81bd41507be81b7bf638b443f95335b19aea6901
Randuit <ste.delfini01@gmail.com>
2024-04-03 21:37:51 +0200

    ✨init

---
+++ Netflix Clone/Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by Delfini, Stefano (VDM) on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
